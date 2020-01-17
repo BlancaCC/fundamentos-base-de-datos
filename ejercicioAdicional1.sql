ea1a56d8913d236347be5e30c4b5e4051c091457
Blanca <blancacano@correo.ugr.es>
2020-01-17 20:22:46 +0100

    ejercicio extra tema 1

---
+++ ejercicioAdicional1.sql
@@ -0,0 +1,11 @@
+-- Base de datos sobre baloncesto
+
+-- Equipos
+CREATE TABLE Equipos (
+codE varchar2(3) NOT NULL,
+nombreE varchar2(5) NOT NULL,
+localidad varchar2(5) NOT NULL,
+entrenador varchar2(5) NOT NULL,
+fecha date  not null
+
+);
