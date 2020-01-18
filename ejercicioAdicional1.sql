2bb8ea167c3d69a8386ec42565225db47ab33c1e
Blanca <blancacano@correo.ugr.es>
2020-01-18 09:54:33 +0100

    ejercicio extra tema acabado sin comprobar

--- ejercicioAdicional1.sql
+++ ejercicioAdicional1.sql
@@ -1,4 +1,5 @@
 -- Base de datos sobre baloncesto
+-- pag 24 del pdf
 
 -- Equipos
 CREATE TABLE Equipos (
@@ -6,6 +7,54 @@ codE varchar2(3) NOT NULL,
 nombreE varchar2(5) NOT NULL,
 localidad varchar2(5) NOT NULL,
 entrenador varchar2(5) NOT NULL,
-fecha date  not null
+fecha DATE NOT NULL,
+
+PRIMARY KEY (codE)
 
 );
+
+
+CREATE TABLE Jugadores (
+codJ VARCHAR2(3) NOT NULL,
+codE  REFERENCES Equipos (codE),
+nombreJ VARCHAR2(10) NOT NULL,
+
+PRIMARY KEY (codJ), 
+
+	
+);
+
+
+CREATE TABLE Encuentros (
+
+ElLocal CONSTRAINT ElLocal_clave_externa_equipos
+	REFERENCES Equipos(codE),
+ElVisitante CONSTRAINT ElVisitante_clave_externa_equipos
+	REFERENCES Equipos(codE),
+	
+fecha DATE,
+pLocal INT DEFAULT 0, 
+pVisitante INT DEFAULT 0
+	   CONSTRAINT puntuacion_positiva CHECK (pLocal >= 0 and pVisititante >= 0), -- mirar si se puede declarar aquí tambien la de pLocal 
+CONSTRAINT clave_primaria PRIMARY KEY (ELocal, EVisitante), 
+
+
+	
+);
+
+-- LA RESTRICCIÓN ES QEU ELLOCAL Y EL VISITANTE SON CLAVE EXTERNA DE LA TABLA ENCUENTROS 
+CREATE TABLE Faltas (
+codJ REFERENCES Jugadores (codJ),
+ELocal REFERENCES Equipos (codE),
+EVisitante REFERENCES Equipos (codE),
+
+FOREING KEY (ELocal, Evisitante)
+	REFERENCES Encuentros (Elocal, EVisitante),
+
+num NUMBER(1)
+    CONSTRAINT faltas_por_defecto DEFAULT 0
+    CONSTRAINT faltas_entre CHECK ( num >= 0 and 5<= num),
+    
+);
+
+-- Añadir constraint, datos y atributos a una tabla 
